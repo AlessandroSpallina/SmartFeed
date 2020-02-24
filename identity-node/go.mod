0e62f9297a9b1f37a21c82bbdb9ca9f7401f7925
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-02-25 00:06:15 +0100

    identity node - live reload fully working

---
+++ identity-node/go.mod
@@ -0,0 +1,5 @@
+module github.com/AlessandroSpallina/SmartFeed
+
+go 1.12
+
+require github.com/gin-gonic/gin v1.5.0
--- identity-node/main.go
+++ identity-node/main.go
@@ -1,13 +1,50 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"os"
 
+	"github.com/gin-gonic/gin"
+)
+
+/*const (
+	httpServerPort = os.Getenv("IDENTITY_NODE_PORT")
+)*/
+
+type config struct {
+	httpServerPort string
+	mqttBrokerHost string
+	mqttBrokerPort string
+}
+
+func getConfigFromEnv() *config {
+	conf := &config{}
+	conf.httpServerPort = os.Getenv("IDENTITY_NODE_PORT")
+	conf.mqttBrokerHost = os.Getenv("MQTT_BROKER_HOST")
+	conf.mqttBrokerPort = os.Getenv("MQTT_BROKER_PORT")
+
+	return conf
+}
+
+/*
+	Identity-Node ha 2 componenti:
+	  * Server HTTP per caso d'uso 1-1a
+		* Client MQTT per caso d'uso 3
+
+	Ci saranno quindi due goroutine dispatcher, uno per protocollo, quindi i task verranno eseguiti in parallelo
+*/
 func main() {
+
+	config := getConfigFromEnv()
+	fmt.Println("Conf:", *config)
+
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": "pongo",
 		})
 	})
+
 	r.Run(":3000")
 }
