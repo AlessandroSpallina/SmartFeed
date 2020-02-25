ce7f7084e6a9b1c904899191551aba2a2e13c526
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-02-25 01:18:04 +0100

    identity node - partial mvc skeleton

--- identity-node/main.go
+++ identity-node/main.go
@@ -1,15 +1,23 @@
+/*Identity-Node ha 2 componenti:
+  - Server HTTP per caso d'uso 1-1a
+	- Client MQTT per caso d'uso 3
+
+Ci saranno quindi due goroutine dispatcher, uno per protocollo, quindi i task verranno eseguiti in parallelo
+*/
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
+
+	//"identity-node/controller/user_controller"
+
+	//"controller"
 
 	"github.com/gin-gonic/gin"
 )
 
-/*const (
-	httpServerPort = os.Getenv("IDENTITY_NODE_PORT")
-)*/
+var conf = getConfigFromEnv()
 
 type config struct {
 	httpServerPort string
@@ -26,25 +34,16 @@ func getConfigFromEnv() *config {
 	return conf
 }
 
-/*
-	Identity-Node ha 2 componenti:
-	  * Server HTTP per caso d'uso 1-1a
-		* Client MQTT per caso d'uso 3
-
-	Ci saranno quindi due goroutine dispatcher, uno per protocollo, quindi i task verranno eseguiti in parallelo
-*/
-func main() {
-
-	config := getConfigFromEnv()
-	fmt.Println("Conf:", *config)
-
+func startHTTPServer() {
 	r := gin.Default()
 
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	//r.GET("/ping", controller.Ping)
 
-	r.Run(":3000")
+	r.Run(":" + conf.httpServerPort)
+}
+
+func main() {
+	log.Println("[IDENTITY] Conf:", *conf)
+
+	startHTTPServer()
 }
