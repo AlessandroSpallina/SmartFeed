9a6a4b65c4f1de75be09209532097a41bb2e72a0
Claudio795 <claudiocurto@hotmai.it>
2020-03-04 23:06:11 +0100

    modified feed-producer environment variables

--- feed-producer/openweather_api.py
+++ feed-producer/openweather_api.py
@@ -10,15 +10,15 @@ import json
 import redis
 
 #recupero i valori di configurazione dal file conf.yml e le variabili d'ambiente dal docker-compose.yml
-with open("conf.yml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
+#with open("conf.yml", 'r') as ymlfile:
+   # cfg = yaml.load(ymlfile)
 
-#API_KEY=os.environ['API_KEY']
-#WEATHER_URL=os.environ['URL']
-API_KEY='79c9118b24b5b9dc61998d484842f0dc'
 
-city=cfg['city']
-state=cfg['state']
+#API_KEY='79c9118b24b5b9dc61998d484842f0dc'
+
+#importo le variabili d'ambiente
+API_KEY=os.environ.get('API_KEY', 'aaaaaaaaaaaaa')
+city=os.environ.get('city', 'londra')
 
 #costruisco il payload da passare alla request per ottenere il json
 payload={'q':city, 'mode':'json', 'appid':API_KEY}
