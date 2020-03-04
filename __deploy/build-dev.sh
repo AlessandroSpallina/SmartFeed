e1f9021124d7cfef4a9595802b54dd8638ca952c
AlessandroSpallina <alessandrospallina1@gmail.com>
2020-03-04 20:29:58 +0100

    clean grafana

--- __deploy/build-dev.sh
+++ __deploy/build-dev.sh
@@ -6,4 +6,4 @@ docker build --rm -t feed-producer:v0 . -f Dockerfile-dev
 
 # grafana dashboard stuff
 # see https://stackoverflow.com/questions/34031397/running-docker-on-ubuntu-mounted-host-volume-is-not-writable-from-container
-chmod go+rw ../../grafana
+#chmod go+rw ../../grafana
