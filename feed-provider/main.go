cca9e410f9c0ff102424e0d770ad1c3b19c9f5c6
manlio <manpuglisi94@gmail.com>
2020-03-06 17:46:28 +0100

    working on feed-provider

---
+++ feed-provider/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/gin-gonic/gin"
+)
+
+type config struct {
+	debug            bool
+	httpServerPort   string
+	mqttBrokerHost   string
+	mqttBrokerPort   string
+	mqttProducerID   string
+	mqttWelcomeTopic string
+}
+
+func getConfigFromEnv() *config {
+	conf := &config{}
+	conf.debug, _ = strconv.ParseBool(os.Getenv("IDENTITY_DEBUG"))
+	conf.httpServerPort = os.Getenv("IDENTITY_HTTP_SERVER_PORT")
+	conf.mqttBrokerHost = os.Getenv("IDENTITY_MQTT_BROKER_HOST")
+	conf.mqttBrokerPort = os.Getenv("IDENTITY_MQTT_BROKER_PORT")
+	conf.mqttProducerID = os.Getenv("IDENTITY_MQTT_PRODUCER_ID")
+	conf.mqttWelcomeTopic = os.Getenv("IDENTITY_MQTT_WELCOME_TOPIC")
+
+	return conf
+}
+
+func startMQTTProducer(brokerHost, brokerPort, clientID, welcomeTopic string, debug bool) {
+	if debug {
+		mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	}
+	mqtt.ERROR = log.New(os.Stdout, "", 0)
+
+	opts := mqtt.NewClientOptions().AddBroker("tcp://" + brokerHost + ":" + brokerPort).SetClientID(clientID)
+	opts.SetKeepAlive(2 * time.Second)
+
+	f := handler.RequestReplyRoutine
+
+	opts.SetDefaultPublishHandler(f)
+
+	opts.SetPingTimeout(1 * time.Second)
+	c := mqtt.NewClient(opts)
+
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	if token := c.Subscribe(welcomeTopic, 0, nil); token.Wait() && token.Error() != nil {
+		log.Println("[IDENTITY] MQTT Subscribe err:", token.Error())
+		os.Exit(1)
+	}
+}
+
+func startHTTPServer(port string, debug bool) {
+	if !debug { // di default gin parte in debug
+		gin.SetMode(gin.ReleaseMode)
+	}
+	router := gin.Default()
+	initializeRoutes(router)
+	router.Run(":" + port)
+}
+
+func main() {
+	conf := getConfigFromEnv()
+	log.Println("[IDENTITY] Conf:", *conf)
+
+	go startMQTTProducer(conf.mqttBrokerHost, conf.mqttBrokerPort, conf.mqttProducerID, conf.mqttWelcomeTopic, conf.debug)
+
+	startHTTPServer(conf.httpServerPort, conf.debug)
+
+	//model.User
+}
---
+++ feed-provider/model/user.go
@@ -0,0 +1,11 @@
+package model
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password,omitempty"`
+	//DateOfBirth time.Time `json:"date-of-birth" time_format:"1994-04-30"`
+	DateOfBirth string `json:"date-of-birth,omitempty"`
+	Gender      string `json:"gender,omitempty"`
+	Phone       string `json:"phone,omitempty"`
+	Email       string `string:"email,omitempty"`
+}
