06dfe0b887e403656cf2204a396d407585fcd1d0
AlessandroSpallina <alessandrospallina1@gmail.com>
2021-02-23 22:11:19 +0100

    updated deploy stuff

--- __deploy/build-dev.sh
+++ __deploy/build-dev.sh
@@ -5,7 +5,7 @@ cd ../../feed-producer
 docker build --rm -t feed-producer:v0 . -f Dockerfile-dev
 
 cd ../feed-provider
-docker build --rm -t feed-provider:v0 . -f Dockerfile-dev
+docker build --rm -t feed-provider:v0.1 . -f Dockerfile-dev
 
 # grafana dashboard stuff
 # see https://stackoverflow.com/questions/34031397/running-docker-on-ubuntu-mounted-host-volume-is-not-writable-from-container
