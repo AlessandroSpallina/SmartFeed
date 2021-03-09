aacaeb3d2e2b4fb384983e1a0adf3a425ef00dc9
AlessandroSpallina <alessandrospallina1@gmail.com>
2021-03-09 15:44:47 +0100

    default values in utils.getconfig

--- feed-provider/utils.py
+++ feed-provider/utils.py
@@ -4,7 +4,7 @@ import os
 def get_config_from_env():
     return {
         "HTTP_SERVER_PORT": int(os.environ.get('PROVIDER_HTTP_SERVER_PORT', 3030)),
-        "MQTT_BROKER_HOST": os.environ.get('PROVIDER_MQTT_BROKER_HOST', 'mqtt-broker'),
+        "MQTT_BROKER_HOST": os.environ.get('PROVIDER_MQTT_BROKER_HOST', 'localhost'),
         "MQTT_BROKER_PORT": int(os.environ.get('PROVIDER_MQTT_BROKER_PORT', 1833)),
         "MQTT_PRODUCER_ID": os.environ.get('PROVIDER_MQTT_PRODUCER_ID', 'provider-0'),
         "MQTT_WELCOME_TOPIC": os.environ.get('PROVIDER_MQTT_WELCOME_TOPIC', 'provider/welcome')
